4a9f7efffac89d9ce974513a138b330d8d5a3d80
dhruv <dhruvilkalathiya04@gmail.com>
2023-10-19 18:14:42 +0100

    My homework week 1

---
+++ src/homework_week_5/Programme_8_DivisionOfTwoNumbers.java
@@ -0,0 +1,17 @@
+package homework_week_5;
+/**
+ * 8. Write a Java program to divide two numbers and print on the screen.
+ * Test Data: 50/3
+ * Expected Output: 16
+ */
+
+public class Programme_8_DivisionOfTwoNumbers {
+    public static void main(String[] args){
+        // The two integers are value1 and values2
+        int values1 = 50;
+        int values2 = 3;
+        int division = values1 / values2;
+        // concatenation
+        System.out.println(" The " + values1 + "is divided by " + values2 + "the answer is " + division);
+    }
+}
---
+++ src/homework_week_5/programme_1_BioData.java
@@ -0,0 +1,22 @@
+package homework_week_5;
+/**
+ * 1. Write a program that prints your BIO Data.
+ */
+
+public class programme_1_BioData {
+    public static void main(String[] args){
+        System.out.println("                  BIO  DATA             ");
+        System.out.println("Name                 : Jane Smith");
+        System.out.println("Mobile               : 079123456789 ");
+        System.out.println("Email                : Janesmith@gmail.com");
+        System.out.println("Gender               : Female ");
+        System.out.println("Date of Birth        : 01.05.2005");
+        System.out.println("Marital Status       : Single");
+        System.out.println("Language Know        : English, Spanish and German");
+        System.out.println("Qualification        : Software Tester");
+        System.out.println("Experience           : Agile Tester with 5 years of experience");
+        System.out.println("Address              : 123,Abc Street , London AB1 2CD");
+        System.out.println("Place                : London");
+        System.out.println("Signature            : JSmith");
+    }
+}
